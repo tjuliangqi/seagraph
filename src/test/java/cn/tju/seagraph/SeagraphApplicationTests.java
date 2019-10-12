0688ad00c08769d7eccaafc06210dd0729d3ace5
wangyares <wangyares@outlook.com>
2019-10-12 12:00:55 +0800

    ceshi + xml

--- src/test/java/cn/tju/seagraph/SeagraphApplicationTests.java
+++ src/test/java/cn/tju/seagraph/SeagraphApplicationTests.java
@@ -1,19 +1,14 @@
 package cn.tju.seagraph;
 
-import cn.tju.seagraph.dao.ConferenceMapper;
 import cn.tju.seagraph.dao.DataMapper;
-import cn.tju.seagraph.dao.StatisticsMapper;
-import cn.tju.seagraph.daomain.Conference;
-import cn.tju.seagraph.daomain.Statistics;
 import cn.tju.seagraph.daomain.data;
-import cn.tju.seagraph.utils.dateUtils;
+import cn.tju.seagraph.daomain.paperMysqlBean;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
-import java.util.ArrayList;
 import java.util.List;
 
 @RunWith(SpringRunner.class)
@@ -21,11 +16,9 @@ import java.util.List;
 public class SeagraphApplicationTests {
 
     @Autowired
-    DataMapper dataMapper;
+    ataMapper dataMapper;D
     @Autowired
-    StatisticsMapper statisticsMapper;
-    @Autowired
-    ConferenceMapper conferenceMapper;
+    cn.tju.seagraph.dao.paperMapper paperMapper;
     @Test
     public void contextLoads() {
     }
@@ -40,51 +33,38 @@ public class SeagraphApplicationTests {
         System.out.println(data.getCount());
         data.setCount(91);
         int k = dataMapper.updateData(data);
-        List<data> list2 = dataMapper.getDataById(2);
+        List<cn.tju.seagraph.daomain.data> list2 = dataMapper.getDataById(5);
         System.out.println(list2.get(0).getCount());
         int j = dataMapper.deleteById(2);
-        List<data> list3 = dataMapper.getDataList();
+        List<cn.tju.seagraph.daomain.data> list3 = dataMapper.getDataList();
         System.out.println(list3.size());
 
     }
-    @Test
-    public void testStatistics(){
-        int i = statisticsMapper.insertStatistics(dateUtils.gainDate());
-        List<Statistics> list1 = statisticsMapper.selectStatisticsByDate(dateUtils.gainDate());
-        System.out.println(list1.size());
-        Statistics statistics = list1.get(0);
-        System.out.println(statistics.getDetail());
-        statistics.setDetail(1);
-        int k = statisticsMapper.updateStatistics(statistics);
-        List<Statistics> list2 = statisticsMapper.selectStatisticsByDate(dateUtils.gainDate());
-        System.out.println(list2.get(0).getDetail());
-        int j = statisticsMapper.deleteByDate(dateUtils.gainDate());
-        List<Statistics> list3 = statisticsMapper.selectStatisticsByDate(dateUtils.gainDate());
-        System.out.println(list3.size());
 
-    }
     @Test
-    public void testConference(){
-        List<String> list = new ArrayList<>();
-        list.add("机器学习");
-        list.add("计算机视觉");
-        Conference conference = new Conference();
-        conference.setDate(dateUtils.gainDate());
-        conference.setHomepage("1");
-        conference.setLabels(list.toString());
-        conference.setLevel("A");
-        conference.setLocation("China");
-        conference.setName("VALSE");
-        conferenceMapper.insertConference(conference);
-        conference.setLevel("B");
-        conference.setId(1);
-        int i = conferenceMapper.updateConference(conference);
-        List<Conference> list1 = conferenceMapper.getAllConference();
-        for (Conference c:list1) {
-            System.out.println(c.getLevel());
+    public void testPaper(){
+        paperMysqlBean paperMysqlBean = paperMapper.getDataById("2f03e746ea3611e9a3d800d861171bd5");
+        System.out.println(paperMysqlBean.toString());
+        System.out.println("测试list");
+        List<cn.tju.seagraph.daomain.paperMysqlBean> list = paperMapper.getDataList();
+        for(cn.tju.seagraph.daomain.paperMysqlBean each:list){
+            System.out.println("*******");
+            System.out.println(each.toString());
         }
-        int k = conferenceMapper.deleteById(2);
-        List<Conference> list2 = conferenceMapper.getAllConference();
-        System.out.println(list2.size());
+        cn.tju.seagraph.daomain.paperMysqlBean t1 = new paperMysqlBean();
+        t1.setUuid("1245422");
+
+        System.out.println("测试delete");
+        paperMapper.deleteById("12454");
+
+        System.out.println("测试insert");
+        System.out.println(t1.toString());
+        paperMapper.insertData(t1);
+
+        System.out.println("测试update");
+        t1.setAbs("wedswerwerf");
+        paperMapper.updateData(t1);
+
+
     }
 }
