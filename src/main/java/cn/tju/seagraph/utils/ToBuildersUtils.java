a6f5c011ce8a1f42695162312f7e3826521436d5
jiagit <884420881@qq.com>
2019-11-12 11:27:48 +0800

    update affiliations(filter)

--- src/main/java/cn/tju/seagraph/utils/ToBuildersUtils.java
+++ src/main/java/cn/tju/seagraph/utils/ToBuildersUtils.java
@@ -1,4 +1,5 @@
 package cn.tju.seagraph.utils;
+import org.elasticsearch.common.util.concurrent.EsRejectedExecutionException;
 import org.elasticsearch.index.query.QueryBuilder;
 import org.elasticsearch.index.query.QueryBuilders;
 import org.json.JSONException;
@@ -43,27 +44,39 @@ public class ToBuildersUtils {
                 builder0 = QueryBuilders.matchQuery(queryField0, value);
             }
             Map map = strToMap(preparaString);
-            String labels = map.get("labels").toString().replace("[", "").replace("]", "").replace("\"", "");
-            String[] labelsToList = labels.split(",");
-            QueryBuilder builder1 = QueryBuilders.matchQuery("labels", labelsToList[0]);
-            for(int i =1;i<labelsToList.length;i++){
-                builder1 = QueryBuilders.boolQuery()
-                        .should(builder1)
-                        .should(QueryBuilders.matchQuery("labels",labelsToList[i]));
+
+            try {
+                String labels = map.get("labels").toString().replace("[", "").replace("]", "").replace("\"", "");
+                String[] labelsToList = labels.split(",");
+                QueryBuilder builder1 = QueryBuilders.matchQuery("labels", labelsToList[0]);
+                for(int i =1;i<labelsToList.length;i++){
+                    builder1 = QueryBuilders.boolQuery()
+                            .should(builder1)
+                            .should(QueryBuilders.matchQuery("labels",labelsToList[i]));
+                }
+                builder0 = QueryBuilders.boolQuery()
+                        .must(builder0)
+                        .must(builder1);
+            }catch (Exception e){
+                System.out.println("No filtration labels");
             }
 
-            String influence = map.get("influence").toString().replace("[", "").replace("]", "").replace("\"", "");
-            String[] influenceToList = influence.split(",");
-            QueryBuilder builder2 = QueryBuilders.matchQuery("influence", influenceToList[0]);
-            for(int j =1;j<influenceToList.length;j++){
-                builder2 = QueryBuilders.boolQuery()
-                        .should(builder2)
-                        .should(QueryBuilders.matchQuery("influence",influenceToList[j]));
+            try {
+                String influence = map.get("influence").toString().replace("[", "").replace("]", "").replace("\"", "");
+                String[] influenceToList = influence.split(",");
+                QueryBuilder builder2 = QueryBuilders.matchQuery("influence", influenceToList[0]);
+                for(int j =1;j<influenceToList.length;j++){
+                    builder2 = QueryBuilders.boolQuery()
+                            .should(builder2)
+                            .should(QueryBuilders.matchQuery("influence",influenceToList[j]));
+                }
+                builder0 = QueryBuilders.boolQuery()
+                        .must(builder0)
+                        .must(builder2);
+            }catch (Exception e){
+                System.out.println("No filtration influence");
             }
-            builder0 = QueryBuilders.boolQuery()
-                    .must(builder0)
-                    .must(builder1)
-                    .must(builder2);
+
         }
 //        System.out.println(builder0);
         return builder0;
@@ -71,15 +84,16 @@ public class ToBuildersUtils {
 
     public static void test() throws JSONException {
         String jsonStr = "{example:{'labels':['oocyte recovery','monospecific antiserum','photosynthetic'],influence:['1','1','1']}}";
-        Map map = strToMap(jsonStr);
-//        System.out.println(map);
+        String jsonStr2 = "{example:{'labels':['oocyte recovery','monospecific antiserum','photosynthetic']}}";
+        Map map = strToMap(jsonStr2);
+        System.out.println(map);
 
         String labels = map.get("labels").toString().replace("[", "").replace("]", "").replace("\"", "");
         String[] strsToList1 = labels.split(",");
         for(int i =1;i<strsToList1.length;i++){
 //            System.out.println(strsToList1[i]);
         }
-        String influence = map.get("influence").toString();
-//        System.out.println(map.get("labels"));
+        Object influence = map.get("influence");
+        System.out.println(influence);
     }
 }
