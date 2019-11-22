92987440c6944c00f4beabd03b3d0aa0ff32e1af
lq296301852 <296301852@qq.com>
2019-11-22 10:06:22 +0800

    authorController update

---
+++ src/main/java/cn/tju/seagraph/utils/affiliationsUtils.java
@@ -0,0 +1,25 @@
+package cn.tju.seagraph.utils;
+
+public class affiliationsUtils {
+    public String strToaffiliations(String name){
+        String affiliation = null;
+        if (name.contains(",")){
+            String[] Affiliations = name.split(",");
+            for(String affil:Affiliations){
+                if (affil.contains("university")){
+                    affiliation = affil;
+                }else {
+                    if (Affiliations.length!=0 && Affiliations.length>=2){
+                        affiliation = Affiliations[2];
+                    }else if (Affiliations.length == 1){
+                        affiliation = Affiliations[0];
+                    }else {
+                        affiliation = null;
+                    }
+                }
+            }
+
+        }
+        return affiliation;
+    }
+}
